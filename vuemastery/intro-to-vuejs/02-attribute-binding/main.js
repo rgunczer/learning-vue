e5f0865b3d71fea4e59a9f9f9064861d5deec49e
rgunczer <rgunczer@gmail.com>
2020-04-09 23:01:34 +0200

    attribute binding

---
+++ vuemastery/intro-to-vuejs/02-attribute-binding/main.js
@@ -0,0 +1,12 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        product: 'Socks',
+        image: './assets/vmSocks.jpg',
+        link: 'http://www.google.com',
+        // inStock: true
+        inventory: 100,
+        showInfo: true,
+        onSale: true
+    }
+});
